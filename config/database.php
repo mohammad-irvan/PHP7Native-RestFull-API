ca4bf2ad29b57d2775423edd071ffe63847443f1
mohammad-irvan <mohirvan066@gmail.com>
2021-03-06 13:20:52 -0800

    commit #1

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php 
+class Database 
+{
+
+private $host	=	"localhost";
+private $db 	=	"db_rest_api";
+private $user 	=	"root";
+private $pass	=	"";
+
+public $conn;
+
+public function getConnection(){
+	$this->conn = null;
+	try{
+		$this->conn = new PDO("mysql:host=".$this->host . ";dbname=" .$this->db, $this->user, $this->pass);
+		$this->conn->exec("set names utf8");
+	}catch(PDOException $exception){
+		echo "Database Berhasil Terhubung: " .$exception->getMessage();
+	}
+	return $this->conn;
+	}
+}
+
+ ?>
